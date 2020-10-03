70c85179e567946215aef2a1ae14d320c00c06f6
Oleksii Shkurupii <Oleksii.Shkurupii@playtech.com>
2020-10-03 10:38:32 +0300

    my test

--- example-jobs/pipeline_play.groovy
+++ example-jobs/pipeline_play.groovy
@@ -8,7 +8,7 @@ String pipelineScript(String filePath) {
 new PipelineJobBuilder(
         name: 'Hello Pipeline With Script',
         description: 'This is a simple pipeline job',
-        pipelineScript: pipelineScript('./playbook.groovy'),
+        pipelineScript: pipelineScript('example-jobs/playbook.groovy'),
         sandboxFlag: true
 ).build(this).with {
     logRotator {
